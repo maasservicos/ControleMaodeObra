d3516aea16a3fc838e323c4f5f2e541028511f39
pablohenr1 <lopeh698@gmail.com>
2026-06-22 12:49:22 -0300

    Troca de Banco

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,9 +1,6 @@
-// js/supabaseClient.js
-
-
-const SUPABASE_URL = "https://ooudzxszovimsmfypckz.supabase.co";
-const SUPABASE_KEY = "sb_publishable_8WyuhLagGwQZFaaWKqXqYA_UJ6toANR";
-
+const SUPABASE_URL = "https://rcubcruyviybpbeqtist.supabase.co";
+const SUPABASE_KEY = "sb_publishable_KcCjsfIFepc3O4YG7CNr8Q_kudJ6Rfi";
+    
 // Cria a conexão e exporta para os outros arquivos usarem
 // (O 'supabase' vem do link CDN no HTML)
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
